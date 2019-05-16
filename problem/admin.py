7b13886afb62bf06c7baf6cc92b1cdb82d6da7b7
rtorrente <romain.torrente@gmail.com>
2019-05-16 15:54:59 +0200

    Update + changement admin

--- problem/admin.py
+++ problem/admin.py
@@ -4,6 +4,23 @@ from .models import *
 
 
 # Register your models here.
-@admin.register(Building, Machine, Ticket)
-class Admin(admin.ModelAdmin):
-    pass
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+    list_display = '__str__', 'type', 'serial_number', 'active', 'building'
+    list_display_links = '__str__',
+    search_fields = '__str__', 'serial_number'
+    list_filter = 'active', 'building', 'type'
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = '__str__', 'email_resp', 'active'
+    list_display_links = '__str__',
+    search_fields = '__str__', 'email_resp'
+    list_filter = 'active',
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = '__str__', 'machine', 'first_name', 'last_name', 'insa_email', 'count_same_email', 'get_state'
+    list_display_links = '__str__',
+    search_fields = '__str__', 'insa_email', 'first_name', 'last_name', 'phone_number', 'room'
+    list_filter = 'state', 'machine', 'problem_type'
