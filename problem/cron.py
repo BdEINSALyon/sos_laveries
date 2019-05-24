eba721360278dc68f6f7ae2e1ce4f546960d34eb
rtorrente <romain.torrente@gmail.com>
2019-05-24 11:47:44 +0200

    Correction erreur

--- problem/cron.py
+++ problem/cron.py
@@ -26,7 +26,7 @@ def notify_pending():
 def old_ticket():
     semaine_3 = timezone.now() - timezone.timedelta(weeks=3)
     semaine_4 = timezone.now() - timezone.timedelta(weeks=4)
-    ticket_email = Ticket.objects.filter(state=1).filter(email_reminder_sent=0).filter(date_treatment__lt=semaine_3)
+    ticket_email = Ticket.objects.filter(state=1).filter(email_reminder_sent=False).filter(date_treatment__lt=semaine_3)
     for ticket in ticket_email:
         date_expire = ticket.date_treatment + timezone.timedelta(weeks=4) - timezone.timedelta(days=1)
         msg_plain = render_to_string('problem/email_reminder_approved.txt',
@@ -35,11 +35,11 @@ def old_ticket():
             'Jetons en attente de récupération',
             msg_plain,
             settings.DEFAULT_FROM_EMAIL,
-            ticket.insa_email,
+            [ticket.insa_email],
         )
-        ticket.email_reminder_sent = 1
+        ticket.email_reminder_sent = True
         ticket.save()
-    ticket_expire = Ticket.objects.filter(state=1).filter(email_reminder_sent=1).filter(date_treatment__lt=semaine_4)
+    ticket_expire = Ticket.objects.filter(state=1).filter(email_reminder_sent=True).filter(date_treatment__lt=semaine_4)
     for ticket in ticket_expire:
         ticket.state = 4
         ticket.save()
